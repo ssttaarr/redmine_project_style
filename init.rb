6fc2fd707e4ed5fa3e6c2e52f17199ee7ad98649
yura <yura.zaplavnov@gmail.com>
2016-05-31 17:01:05 +0200

    adjusted plugin name

--- init.rb
+++ init.rb
@@ -4,10 +4,10 @@ require 'redmine_project_style_layout_hook'
 Redmine::Plugin.register :redmine_project_style do
   name 'Redmine Project Style'
   author 'Yura Zaplavnov'
-  description 'Allows to set a visual style per project using project identifier. (cloned from redmine_projectstyle and modified to make it compatible with latest version of redmine)'
+  description 'Allows to set a visual style per project using project identifier. (cloned from redmine_project_style and modified to make it compatible with latest version of redmine)'
   version '3.0'
   url 'https://github.com/xeagle2/redmine_project_style'
   author_url 'https://github.com/xeagle2'
 
-  permission :redmine_projectstyle, {:redmine_projectstyle => []}, :public => true
+  permission :redmine_project_style, {:redmine_project_style => []}, :public => true
 end
